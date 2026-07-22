41964527e4f64bb33495b6319bbf33386a36aea3
roeiroeiyifrah-wq <roeiroeiyifrah@gmail.com>
2026-07-22 11:40:20 +0300

    Add wheel command for Discord bot

--- commands/wheel.js
+++ commands/wheel.js
@@ -1,1 +1,30 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("wheel")
+    .setDescription("פתיחת גלגל המזל"),
+
+  async execute(interaction) {
+
+    const WHEEL_CHANNEL_ID =
+      "1529158985725251624";
+
+
+    if (interaction.channel.id !== WHEEL_CHANNEL_ID) {
+
+      return interaction.reply({
+        content: "❌ הפקודה רק בחדר הגלגל",
+        ephemeral: true
+      });
+
+    }
+
+
+    await interaction.reply({
+      content: "🎡 הגלגל כבר נמצא בחדר!",
+      ephemeral: true
+    });
+
+  }
+};
