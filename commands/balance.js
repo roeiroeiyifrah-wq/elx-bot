0c3acfd117c900ad5907db653ff5bc72c6953c87
roeiroeiyifrah-wq <roeiroeiyifrah@gmail.com>
2026-07-22 11:39:42 +0300

    Add balance command to check user points

--- commands/balance.js
+++ commands/balance.js
@@ -1,1 +1,27 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { loadData } = require("../utils/save");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("balance")
+    .setDescription("בדיקת כמות נקודות"),
+
+  async execute(interaction) {
+
+    const points = loadData(
+      "points.json",
+      {}
+    );
+
+
+    const amount =
+      points[interaction.user.id] || 0;
+
+
+    await interaction.reply({
+      content: `💎 יש לך ${amount} נקודות`,
+      ephemeral: true
+    });
+
+  }
+};
