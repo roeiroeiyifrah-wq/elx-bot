d39c7754ea7328708340466c63661df666dc3b64
roeiroeiyifrah-wq <roeiroeiyifrah@gmail.com>
2026-07-22 11:37:19 +0300

    Add loadData and saveData functions to save.js

--- utils/save.js
+++ utils/save.js
@@ -1,1 +1,25 @@
+const fs = require("fs");
 
+function loadData(file, defaultData = {}) {
+  if (fs.existsSync(file)) {
+    return JSON.parse(
+      fs.readFileSync(file, "utf8")
+    );
+  }
+
+  return defaultData;
+}
+
+
+function saveData(file, data) {
+  fs.writeFileSync(
+    file,
+    JSON.stringify(data, null, 2)
+  );
+}
+
+
+module.exports = {
+  loadData,
+  saveData
+};
